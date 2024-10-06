6b1d03055057740eed45f009e00d063aaaaac80f
Lotfi MEZIANI <lotfi.mez7@gmail.com>
2024-10-06 18:46:17 +0200

    Initialize python project

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello World!")
+
+# The following condition checks if this script is being executed directly.
+# If so, it calls the main() function. If this script is imported as a module
+# in another file, this block won't run unless explicitly called.
+if __name__ == "__main__":
+    main()
